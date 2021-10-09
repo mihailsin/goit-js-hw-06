48bf23bb2246d3234fabafb8ea363d3950982aa7
Mihail Sinkov <mihailsin88@gmail.com>
2021-10-09 21:09:05 +0300

    first task is done

---
+++ src/js/task-01.js
@@ -0,0 +1,15 @@
+const categoriesRef = document.querySelector('#categories');
+const classesRef = categoriesRef.children;
+console.log(`Number of categories: ${classesRef.length}`);
+
+const animalsListRef = document.querySelector('.item');
+console.log(`Category: ${animalsListRef.firstElementChild.textContent}`);
+console.log(`Elements: ${animalsListRef.firstElementChild.nextElementSibling.children.length}`);
+
+const productListRef = animalsListRef.nextElementSibling;
+console.log(`Category: ${productListRef.firstElementChild.textContent}`);
+console.log(`Elements: ${productListRef.firstElementChild.nextElementSibling.children.length}`);
+
+const techListRef = productListRef.nextElementSibling;
+console.log(`Category: ${techListRef.firstElementChild.textContent}`);
+console.log(`Elements: ${techListRef.firstElementChild.nextElementSibling.children.length}`);
