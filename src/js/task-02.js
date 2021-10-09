a7969a72ef5bed5c42715e898ef1a111360be4c2
Mihail Sinkov <mihailsin88@gmail.com>
2021-10-09 21:39:32 +0300

    second task accomplished

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const ingredientsListRef = document.querySelector('#ingredients');
+
+const items = ingredients.map(ingredient => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = ingredient;
+  itemRef.classList.add('item');
+  return itemRef;
+});
+
+ingredientsListRef.append(...items);
