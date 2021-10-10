fd3e0b6dee0e4c81e4c5eeaa0e7dbf0affadb0c5
Mihail Sinkov <mihailsin88@gmail.com>
2021-10-10 22:51:11 +0300

    task №9 is done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
+const body = document.firstElementChild.firstElementChild.nextElementSibling;
+const span = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorBtn = document.querySelector('.change-color');
+const changeColor = event => {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+};
+changeColorBtn.addEventListener('click', changeColor);
