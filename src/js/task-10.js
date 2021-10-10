e60bd3ee506c6782dd52d360aed1955d5e25125f
Mihail Sinkov <mihailsin88@gmail.com>
2021-10-11 00:12:32 +0300

    started 10th task

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const boxesRef = document.querySelector('#boxes');
+const createBoxes = amount => {
+  for (let i = 1; i <= amount; i += 1) {
+    console.log(document.createElement('div'));
+  }
+};
+createBoxes(5);
