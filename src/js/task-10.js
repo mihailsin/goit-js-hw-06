76e112f232853dc03547d903a3a75074784759d7
Mihail Sinkov <mihailsin88@gmail.com>
2021-10-11 17:02:05 +0300

    10th task is done ... almost ...

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,10 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+let quantity;
 const boxesRef = document.querySelector('#boxes');
-const createBoxes = amount => {
-  for (let i = 1; i <= amount; i += 1) {
-    console.log(document.createElement('div'));
+const form = document.querySelector('#controls');
+const input = form.firstElementChild;
+const createBtn = input.nextElementSibling;
+const destroyBtn = createBtn.nextElementSibling;
+
+let arrayOfBoxes = [];
+const setOutput = event => {
+  quantity = event.currentTarget.value;
+};
+const onRemove = event => {
+  boxesRef.innerHTML = '';
+  arrayOfBoxes = [];
+};
+const onCreate = event => {
+  let createdBox;
+  for (let i = 1; i <= quantity; i += 1) {
+    createdBox = document.createElement('div');
+    createdBox.style.width = '30px';
+    createdBox.style.height = '30px';
+    createdBox.style.backgroundColor = getRandomHexColor();
+    arrayOfBoxes.push(createdBox);
+    boxesRef.append(...arrayOfBoxes);
   }
 };
-createBoxes(5);
+
+input.addEventListener('change', setOutput);
+createBtn.addEventListener('click', onCreate);
+destroyBtn.addEventListener('click', onRemove);
