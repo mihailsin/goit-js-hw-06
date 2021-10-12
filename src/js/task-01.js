69d193b8e4d7c0e1f19489f1493aecf4550b870e
Mihail Sinkov <mihailsin88@gmail.com>
2021-10-12 22:09:25 +0300

    1st task fix

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,14 +3,25 @@ const classesRef = categoriesRef.children;
 console.log(`Number of categories: ${classesRef.length}`);
 
 const animalsListRef = document.querySelector('.item');
-
-console.log(`Category: ${animalsListRef.firstElementChild.textContent}`);
-console.log(`Elements: ${animalsListRef.firstElementChild.nextElementSibling.children.length}`);
-
 const productListRef = animalsListRef.nextElementSibling;
-console.log(`Category: ${productListRef.firstElementChild.textContent}`);
-console.log(`Elements: ${productListRef.firstElementChild.nextElementSibling.children.length}`);
-
 const techListRef = productListRef.nextElementSibling;
-console.log(`Category: ${techListRef.firstElementChild.textContent}`);
-console.log(`Elements: ${techListRef.firstElementChild.nextElementSibling.children.length}`);
+
+function logResults(list) {
+  console.log(`Category: ${list.firstElementChild.textContent}`);
+  console.log(`Elements: ${list.firstElementChild.nextElementSibling.children.length}`);
+}
+logResults(animalsListRef);
+logResults(productListRef);
+logResults(techListRef);
+
+// const animalsListRef = document.querySelector('.item');
+// console.log(`Category: ${animalsListRef.firstElementChild.textContent}`);
+// console.log(`Elements: ${animalsListRef.firstElementChild.nextElementSibling.children.length}`);
+
+// const productListRef = animalsListRef.nextElementSibling;
+// console.log(`Category: ${productListRef.firstElementChild.textContent}`);
+// console.log(`Elements: ${productListRef.firstElementChild.nextElementSibling.children.length}`);
+
+// const techListRef = productListRef.nextElementSibling;
+// console.log(`Category: ${techListRef.firstElementChild.textContent}`);
+// console.log(`Elements: ${techListRef.firstElementChild.nextElementSibling.children.length}`);
